c49dc1a395e80e79d4a61f8a757b8c51fb62b9f6
JUNG, SUN WOO <110346165+sunoo2468@users.noreply.github.com>
2026-09-12 15:19:12 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.02 ms, Memory: 11.3 MB -BaekjoonHub

--- Python/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ Python/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,6 +1,6 @@
 def solution(n, words):
     answer = []
-    used_words = []
+    used_words = set()
     
     i = 0
     for _ in range(len(words)):
@@ -11,4 +11,21 @@ def solution(n, words):
             return [i % n + 1, i // n + 1]
         i += 1
             
+    return [0, 0]
+
+def solution(n, words):
+    used_words = set()
+
+    for i in range(len(words)):
+
+        # 이미 나온 단어
+        if words[i] in used_words:
+            return [(i % n) + 1, (i // n) + 1]
+
+        # 앞 단어의 마지막 글자와 연결되지 않음
+        if i > 0 and words[i - 1][-1] != words[i][0]:
+            return [(i % n) + 1, (i // n) + 1]
+
+        used_words.add(words[i])
+
     return [0, 0]
